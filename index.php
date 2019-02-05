c055ad01f89964f952b15f74cdbf42256d9f916c
victorgarciasisi <45923592+victorgarciasisi@users.noreply.github.com>
2019-02-05 17:00:05 +0100

    index.php
    Init

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+	//index.php
+
+	//ubicacion csv
+	define('CSV', '100Records.csv');
+
+	//leer csv
+	$readCsv = array_map('str_getcsv', file(CSV));
+?>
+
+<html>
+<table border="1">
+
+<?php
+	//recorremos filas del csv
+	foreach ($readCsv as $itemCsv) {
+		echo '<tr>';
+		
+		//recorremos cerdas del csv
+		foreach ($itemCsv as $elementoItemCSV) {
+			echo '<td>';
+			
+			//mostramos la celda
+			echo $elementoItemCSV;
+			
+			echo '</td>';
+		}
+		
+		echo '</tr>';
+
+	}
+?>
+
+</table>
+</html>
